3dce409eac6047bd52752bd2ba967a51faed7072
Will Gauvin <wgauvin@thoughtworks.com>
2017-07-26 16:04:29 +1000

    [Will] fixed imports

--- src/main/java/com/thoughtworks/beachhack/repository/DrinkRepository.java
+++ src/main/java/com/thoughtworks/beachhack/repository/DrinkRepository.java
@@ -1,23 +1,13 @@
 package com.thoughtworks.beachhack.repository;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.ClientConfigurationFactory;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.QueryRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.thoughtworks.beachhack.model.DrinkStock;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * Created by wgauvin on 26/7/17.
