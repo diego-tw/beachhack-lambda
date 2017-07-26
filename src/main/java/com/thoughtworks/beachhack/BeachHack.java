5745b060a633d6c71c5363b2ae8e58975c43d097
Diego Tognola <dtognola@thoughtworks.com>
2017-07-26 14:12:46 +1000

    adding logging of what is returned

--- src/main/java/com/thoughtworks/beachhack/BeachHack.java
+++ src/main/java/com/thoughtworks/beachhack/BeachHack.java
@@ -24,6 +24,7 @@ public class BeachHack implements RequestHandler<DrinkStock, Map<String, Integer
             inventory.updateInventory(delta.getDrinkName(), delta.getQuantity());
         }
 
+        logger.log("Returning inventory: " + inventory.getInventoryMap());
         return inventory.getInventoryMap();
     }
 
