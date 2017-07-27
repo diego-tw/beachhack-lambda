575ef084788c5f28c2886e400a6be6de3c8b4078
Will Gauvin <wgauvin@thoughtworks.com>
2017-07-27 15:07:51 +1000

    [Will] added stub service to send off alerts for stock level

---
+++ src/main/java/com/thoughtworks/beachhack/service/DrinkStockAlertService.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.beachhack.service;
+
+import com.thoughtworks.beachhack.model.DrinkStock;
+
+/**
+ * Created by wgauvin on 27/7/17.
+ */
+public class DrinkStockAlertService {
+
+    public void alertLowStockLevel(DrinkStock stock) {
+
+    }
+
+}
