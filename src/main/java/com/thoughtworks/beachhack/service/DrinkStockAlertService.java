5176d89a3d4a754249a4c217022bccf84d96d5da
Will Gauvin <wgauvin@thoughtworks.com>
2017-07-27 15:52:56 +1000

    [Will] added code to publish events out to SNS

--- src/main/java/com/thoughtworks/beachhack/service/DrinkStockAlertService.java
+++ src/main/java/com/thoughtworks/beachhack/service/DrinkStockAlertService.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.beachhack.service;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishRequest;
 import com.thoughtworks.beachhack.model.DrinkStock;
 
 /**
@@ -7,8 +10,21 @@ import com.thoughtworks.beachhack.model.DrinkStock;
  */
 public class DrinkStockAlertService {
 
-    public void alertLowStockLevel(DrinkStock stock) {
+    private final AmazonSNS client;
+    private final String topicArn;
 
+    public DrinkStockAlertService() {
+        this.client = AmazonSNSClientBuilder.defaultClient();
+        this.topicArn = client.createTopic("low_stock_alert").getTopicArn();
+    }
+
+    public void alertLowStockLevel(DrinkStock stock) {
+        PublishRequest publishRequest = new PublishRequest()
+                .withTopicArn(topicArn)
+                .withSubject(String.format("Stock level for %s is low", stock.getDrinkName()))
+                .withMessage(String.format("Current stock level for %s is %d", stock.getDrinkName(), stock.getQuantity()));
+
+        client.publish(publishRequest);
     }
 
 }
