713714d417babd9e6384067d996991319ea1029c
Will Gauvin <wgauvin@thoughtworks.com>
2017-08-08 15:56:14 +1000

    [Will] adding a stub of the ElasticSearchClient. This allows the new LogService to talk to it

---
+++ src/main/java/com/thoughtworks/beachhack/service/ElasticSearchClient.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.beachhack.service;
+
+public class ElasticSearchClient {
+
+    public void sendDocumentToElasticSearch(String indexName, String id, String payload) {
+        // TODO - add implementation
+    }
+
+}
